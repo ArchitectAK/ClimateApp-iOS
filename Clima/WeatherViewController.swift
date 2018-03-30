6d6f9fb53f91c97415905a71c3428302fb70ce12
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-03-30 17:48:20 +0800

    implemented didLocationUdate

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -83,11 +83,26 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location  = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude) , latitude = \(location.coordinate.latitude)")
+            let latitude = String (location.coordinate.latitude)
+            let longitude = String (location.coordinate.longitude)
+            let params : [String : String] = ["lat" : latitude, "long" :  longitude, "appid" : APP_ID]
+            
+            
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
     
